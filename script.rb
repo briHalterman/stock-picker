addcbb9f672aac5b827f4ac6f82bf35f919e2c82
Brittany Halterman <halterma@gmail.com>
2025-04-28 17:12:11 -0400

    Initial commit

---
+++ script.rb
@@ -0,0 +1,14 @@
+# Compare each day's price with every following day's price, and track the pair that gives the highest profit (or smallest loss) based on purchase price subtracted from sell price
+
+
+def stock_picker(daily_prices)
+  # Initialize variables to track best days and profit
+    # Loop through each day as a purchase day
+      # Calculate profit (sell price - buy price)
+      # if profit is is highest so far
+        # Update best pair
+  # Return best pair
+
+end
+
+puts stock_picker([17,3,6,9,15,8,6,1,10]) == [1, 4]
