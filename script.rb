f30aa3193cd79f8caa6287f9b1d0f4002ca4c714
Brittany Halterman <halterma@gmail.com>
2025-04-28 17:45:47 -0400

    Implement stock_picker method
    - Use nested loops to compare prices on all valid buy and sell day pairs
    - Store potential optimal days and profit
    - Return optimal days

--- script.rb
+++ script.rb
@@ -3,12 +3,36 @@
 
 def stock_picker(daily_prices)
   # Initialize variables to track best days and profit
-    # Loop through each day as a purchase day
-      # Calculate profit (sell price - buy price)
-      # if profit is is highest so far
-        # Update best pair
-  # Return best pair
+  optimal_purchase_day = 0
+  optimal_sell_day = 0
+  max_profit = 0
 
+  # Loop through each day as a purchase day
+  daily_prices.each_with_index do |purchase_price, purchase_day|
+
+    # Loop through subsequent days
+    daily_prices.each_with_index do |sell_price, sell_day|
+      if sell_day <= purchase_day
+        next
+      end
+
+      # Calculate profit (sell price - buy price)
+      potential_profit = sell_price - purchase_price
+
+      # if profit is is highest so far
+      if potential_profit > max_profit
+        max_profit = potential_profit
+
+        # Update best pair
+        optimal_purchase_day = purchase_day
+        optimal_sell_day = sell_day
+      end
+    end
+  end
+
+  # Return best pair
+  optimal_days =  [optimal_purchase_day, optimal_sell_day]
+  optimal_days
 end
 
 puts stock_picker([17,3,6,9,15,8,6,1,10]) == [1, 4]
